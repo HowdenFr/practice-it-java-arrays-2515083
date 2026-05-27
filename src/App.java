1a75c724cda596a5d84e8af0b4a697f0f03c15b4
Frank Howden <fhowden04@gmail.com>
2026-05-27 15:49:25 +0000

    lottery ball changes

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
-  
+
   public static void main(String[] args) {
-    
+    // double [] lotteryNums = new double[5];
+    double[] lotteryNums = { 34, 52, 67, 12, 35 };
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2] = 40;
+    System.out.println(lotteryNums[2]);
+
   }
 }
