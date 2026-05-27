79ec2ad768864c15a4c650579233de56ae4e3d42
Frank Howden <fhowden04@gmail.com>
2026-05-27 16:01:15 +0000

    iterations of multiple arrays

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class App {
 
   public static void main(String[] args) {
@@ -6,6 +8,31 @@ public class App {
     System.out.println(lotteryNums[2]);
     lotteryNums[2] = 40;
     System.out.println(lotteryNums[2]);
+
+    //for loops
+    int [] arr = new int[] {92, 12,38,74};
+
+    for (int i=0; i<arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    //going backward
+                                  //can change to bigger jump
+    for (int i=arr.length-1; i>=0; i--){
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    //for each for loop
+    for (int item: arr) {
+      System.out.println(item);
+    }
+    System.out.println();
+
+    //java streams
+    Arrays.stream(arr).forEach(System.out::println);
+
 
   }
 }
